2f1daaff3ad9725e34319caf06a1fbe0b82e86a3
Luke Sandell <lasandell@gmail.com>
2013-08-20 21:28:35 -0700

    Fix formatting

--- luketopia+fsharp/bowling_golf.fsx
+++ luketopia+fsharp/bowling_golf.fsx
@@ -1,8 +1,9 @@
-// Minmalist code-golf version of bowling scoring problem.
+// Minimalist code-golf version of bowling scoring problem.
 // It pattern matches a flattened list of the frames (rolls).
 // The flattening approach is helpful because the follow-up rolls
 // to a spare or strike may or may not be in the same frame due 
 // to those extra bonus rolls at the end of the 10th frame.
+
 let rec score frames =
     if frames = [] then 0 else
     match frames |> List.collect id with
@@ -14,18 +15,18 @@ let rec score frames =
 
 // Print some actual versus expected tuples to test my scoring function
 [
-    0, []
-    0, [[]]
-    6, [[5;0];[1]];
-    10, [[10]];
-    12, [[10];[1]];
-    14, [[10];[1;1]];
-    10, [[5;5]];
-    20, [[5;5];[5]];
-    25, [[5;5];[5;5]];
-    0, [[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0]];
-    20, [[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1]];
-    110, [[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10;10]];
+    0,   []
+    0,   [[]]
+    6,   [[5;0];[1]]
+    10,  [[10]]
+    12,  [[10];[1]]
+    14,  [[10];[1;1]]
+    10,  [[5;5]]
+    20,  [[5;5];[5]]
+    25,  [[5;5];[5;5]]
+    0,   [[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0];[0;0]]
+    20,  [[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1];[1;1]]
+    110, [[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10];[0;10;10]]
     300, [[10];[10];[10];[10];[10];[10];[10];[10];[10];[10;10;10]]
 ]
 |> Seq.map (fun (x, y) -> x, score y)
